a97ff8912e7f6b4fa30eb7f29a855b7a4685d6d3
milkyfuckinway <0-0_0@bk.ru>
2022-12-17 15:11:31 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-import './modules/menu.js';
-import './modules/slider.js';
-import './modules/map.js';
-
-
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9tb2R1bGVzL21lbnUuanMnO1xyXG5pbXBvcnQgJy4vbW9kdWxlcy9zbGlkZXIuanMnO1xyXG5pbXBvcnQgJy4vbW9kdWxlcy9tYXAuanMnO1xyXG5cclxuIl0sImZpbGUiOiJtYWluLmpzIn0=
+import"./modules/menu.js";import"./modules/slider.js";import"./modules/map.js";
